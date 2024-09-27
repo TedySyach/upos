d0b9879bf3b7bb3076372ad69a60feba18571abb
Tedy Syach <tedyysyyach@gmail.com>
2024-09-27 16:10:54 +0700

    Refaktor

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,6 @@ Route::get('/user', function (Request $request) {
 
 Route::post('login', [AuthController::class, 'login']);
 
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
 Route::apiResource('product', ProductController::class)->middleware('auth:sanctum');
